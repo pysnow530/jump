2607a9a7a52dd70647464b80f86e002f4be53c04
pysnow530 <pysnow530@163.com>
2025-08-09 18:04:45 +0800

    feat: 改为自动开启摄像头模式

--- script.js
+++ script.js
@@ -12,17 +12,18 @@
 // limitations under the License.
 // import { PoseLandmarker, FilesetResolver, DrawingUtils } from "https://cdn.skypack.dev/@mediapipe/tasks-vision@0.10.0";
 import { PoseLandmarker, FilesetResolver, DrawingUtils } from "./node_modules/@mediapipe/tasks-vision/vision_bundle.mjs";
-const demosSection = document.getElementById("demos");
-let poseLandmarker = undefined;
-let runningMode = "IMAGE";
-let enableWebcamButton;
-let webcamRunning = false;
+
+let poseLandmarker = null;
 const videoWidth = window.innerWidth / 2;
-// Before we can use PoseLandmarker class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
-const createPoseLandmarker = async () => {
-    // const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
+
+const video = document.getElementById("webcam");
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+const drawingUtils = new DrawingUtils(canvasCtx);
+
+// 加载模型, 打开摄像头
+const loadModelAndCamera = async () => {
+    // 加载模型
     const vision = await FilesetResolver.forVisionTasks("/node_modules/@mediapipe/tasks-vision/wasm");
     poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
         baseOptions: {
@@ -30,52 +31,29 @@ const createPoseLandmarker = async () => {
             modelAssetPath: `/models/pose_landmarker_lite.task`,
             delegate: "GPU"
         },
-        runningMode: runningMode,
+        runningMode: 'VIDEO',
         numPoses: 2
     });
-    demosSection.classList.remove("invisible");
-};
-createPoseLandmarker();
-/********************************************************************
-// Demo 2: Continuously grab image from webcam stream and detect it.
-********************************************************************/
-const video = document.getElementById("webcam");
-const canvasElement = document.getElementById("output_canvas");
-const canvasCtx = canvasElement.getContext("2d");
-const drawingUtils = new DrawingUtils(canvasCtx);
-// Check if webcam access is supported.
-const hasGetUserMedia = () => { var _a; return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia); };
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
-if (hasGetUserMedia()) {
-    enableWebcamButton = document.getElementById("webcamButton");
-    enableWebcamButton.addEventListener("click", enableCam);
-}
-else {
-    console.warn("getUserMedia() is not supported by your browser");
-}
-// Enable the live webcam view and start detection.
-function enableCam(_) {
-    if (!poseLandmarker) {
-        console.log("Wait! poseLandmaker not loaded yet.");
-        return;
-    }
-    if (webcamRunning === true) {
-        webcamRunning = false;
-        enableWebcamButton.innerText = "ENABLE PREDICTIONS";
+
+    // 打开摄像头
+    const hasGetUserMedia = () => { var _a; return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia); };
+    if (hasGetUserMedia()) {
+        enableCam();
     }
     else {
-        webcamRunning = true;
-        enableWebcamButton.innerText = "DISABLE PREDICTIONS";
+        alert("getUserMedia() is not supported by your browser");
     }
-    // getUsermedia parameters.
-    const constraints = {
-        video: true
-    };
+};
+loadModelAndCamera();
+
+// Enable the live webcam view and start detection.
+function enableCam() {
     // Activate the webcam stream.
-    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
         video.srcObject = stream;
+        // 监听视频帧
         video.addEventListener("loadeddata", predictWebcam);
+        // 重置视频大小
         video.addEventListener('loadedmetadata', function() {
             const videoHeight = videoWidth / video.videoWidth * video.videoHeight;
             canvasElement.style.height = videoHeight + 'px';
@@ -99,11 +77,6 @@ let hasUpList = _.range(33).map(_ => false);
 let minYList = null
 
 async function predictWebcam() {
-    // Now let's start detecting the stream.
-    if (runningMode === "IMAGE") {
-        runningMode = "VIDEO";
-        await poseLandmarker.setOptions({ runningMode: "VIDEO" });
-    }
     let startTimeMs = performance.now();
     if (lastVideoTime !== video.currentTime) {
         lastVideoTime = video.currentTime;
@@ -158,10 +131,9 @@ async function predictWebcam() {
             addDataPoint(_.map(nodeData, _.property('y')));
         });
     }
+
     // Call this function again to keep predicting when the browser is ready.
-    if (webcamRunning === true) {
-        window.requestAnimationFrame(predictWebcam);
-    }
+    window.requestAnimationFrame(predictWebcam);
 }
 
 // debug panel
